907e7b71dc28287dc61712f14a5542c884b83742
DR34M3R-T <1731428261@qq.com>
2024-06-18 10:15:56 +0800

    add effect

--- control.js
+++ control.js
@@ -1,5 +1,126 @@
-found = false
+found = false;
 document.addEventListener("DOMContentLoaded", function () {
+
+    // orig effect player code from dist_cb55cecae636fcf3b460c013a34f03a9a60edba3-a1263fae866c7b95c8ebc1fc56f20f7a_bundle.js
+    o = function (n, o, i) {
+        var r = n
+            , a = document.getElementById("effect");
+        // console.log("position.x", e.x),
+        console.log("cPosition.x", n.x),
+            console.log("currentPosition", r);
+        var s = document.createElement("a-entity");
+        s.setAttribute("position", n),
+            s.setAttribute("scale", o),
+            s.setAttribute("particle-system", {
+                color: "#FFFFFF",
+                size: "" + i,
+                texture: "./assets/star/Blue_logoEffect-37g235u4w1.png",
+                positionSpread: "0 0 0",
+                rotationAxis: "x",
+                direction: "-1",
+                duration: "0.3",
+                rate: "0",
+                particleCount: "25",
+                maxParticleCount: "25",
+                maxAge: "1.5",
+                accelerationValue: "0, -0.2, 0",
+                accelerationSpread: "-0.15 -0.15 -0.15",
+                velocityValue: "0 0 0",
+                velocitySpread: "-1 -1 -1",
+                blending: 2
+            }),
+            a.appendChild(s);
+        var l = document.createElement("a-entity");
+        l.setAttribute("position", n),
+            l.setAttribute("scale", o),
+            l.setAttribute("particle-system", {
+                color: "#FFFFFF",
+                size: "" + i,
+                texture: "./assets/star/Green_logoEffect-i26ci1o1ua.png",
+                positionSpread: "0 0 0",
+                rotationAxis: "x",
+                direction: "-1",
+                duration: "0.3",
+                rate: "0",
+                particleCount: "25",
+                maxParticleCount: "25",
+                maxAge: "1.5",
+                accelerationValue: "0, -0.2, 0",
+                accelerationSpread: "-0.1 -0.1 -0.1",
+                velocityValue: "0 0 0",
+                velocitySpread: "-1 -1 -1",
+                blending: 2
+            }),
+            a.appendChild(l);
+        var c = document.createElement("a-entity");
+        c.setAttribute("position", n),
+            c.setAttribute("scale", o),
+            c.setAttribute("particle-system", {
+                color: "#FFFFFF",
+                size: "" + i,
+                texture: "./assets/star/Red_logoEffect-p41hd3cbel.png",
+                positionSpread: "0 0 0",
+                rotationAxis: "x",
+                direction: "-1",
+                duration: "0.3",
+                rate: "0",
+                particleCount: "25",
+                maxParticleCount: "25",
+                maxAge: "1.5",
+                accelerationValue: "0, -0.2, 0",
+                accelerationSpread: "-0.1 -0.1 -0.1",
+                velocityValue: "0 0 0",
+                velocitySpread: "-1 -1 -1",
+                blending: 2
+            }),
+            a.appendChild(c);
+        var d = document.createElement("a-entity");
+        d.setAttribute("position", n),
+            d.setAttribute("scale", o),
+            d.setAttribute("particle-system", {
+                color: "#FFFFFF",
+                size: "" + i,
+                texture: "./assets/star/white_LogoEffect-h1obtu7c9x.png",
+                positionSpread: "0 0 0",
+                rotationAxis: "x",
+                direction: "-1",
+                duration: "0.3",
+                rate: "0",
+                particleCount: "25",
+                maxParticleCount: "25",
+                maxAge: "1.5",
+                accelerationValue: "0, -0.2, 0",
+                accelerationSpread: "-0.1 -0.1 -0.1",
+                velocityValue: "0 0 0",
+                velocitySpread: "-1 -1 -1",
+                blending: 2
+            }),
+            a.appendChild(d);
+        var u = document.createElement("a-entity");
+        u.setAttribute("position", n),
+            u.setAttribute("scale", o),
+            u.setAttribute("particle-system", {
+                color: "#FFFFFF",
+                size: "" + i,
+                texture: "./assets/star/yellow_logo-w5xm846a3i.png",
+                positionSpread: "0 0 0",
+                rotationAxis: "x",
+                direction: "-1",
+                duration: "0.3",
+                rate: "0",
+                particleCount: "25",
+                maxParticleCount: "25",
+                maxAge: "1.5",
+                accelerationValue: "0, -0.2, 0",
+                accelerationSpread: "-0.1 -0.1 -0.1",
+                velocityValue: "0 0 0",
+                velocitySpread: "-1 -1 -1",
+                blending: 2
+            }),
+            console.log("Particle size:", i),
+            a.appendChild(u)
+    }
+
     const sceneEl = document.querySelector('a-scene');
     let arSystem;
     sceneEl.addEventListener('loaded', function () {
@@ -16,9 +137,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     // detect target found
     Target.addEventListener("targetFound", event => {
+        console.log("target found");
         if (found) return;
         found = true;
-        console.log("target found");
+        console.log("first found, playing.");
+
+        // play sound
+        var sound = document.getElementById('anyaVoice');
+        sound.play();
+
+        // play animation
         const a_anya = document.getElementById('a-anya');
         const a_snow = document.getElementById('a-snow');
         const ani_data = {
@@ -27,8 +155,56 @@ document.addEventListener("DOMContentLoaded", function () {
         };
         a_anya.setAttribute('animation-mixer', ani_data);
         a_snow.setAttribute('animation-mixer', ani_data);
-        var sound = document.getElementById('anyaVoice');
-        sound.play();
+
+        // effect timeout
+        setTimeout((function() {
+            o({
+                x: 0,
+                y: 2.06574,
+                z: -.139365
+            }, {
+                x: .5,
+                y: .5,
+                z: .5
+            }, .15)
+        }
+        ), 12550),
+        setTimeout((function() {
+            o({
+                x: 0,
+                y: 2.42392,
+                z: -.052186
+            }, {
+                x: 1,
+                y: 1,
+                z: 1
+            }, .45)
+        }
+        ), 12960),
+        setTimeout((function() {
+            o({
+                x: .733442,
+                y: 1.70214,
+                z: .078421
+            }, {
+                x: 1,
+                y: 1,
+                z: 1
+            }, .45)
+        }
+        ), 13340),
+        setTimeout((function() {
+            o({
+                x: -.781238,
+                y: 1.76574,
+                z: -.139365
+            }, {
+                x: 1,
+                y: 1,
+                z: 1
+            }, .45)
+        }
+        ), 13780);
     });
     // detect target lost
     Target.addEventListener("targetLost", event => {
